785df9fb0c619dd4922b2727f512414556418848
Sebastian Krümling <sebastian.kruemling@gmail.com>
2018-07-18 09:48:07 +0200

    updated versions

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 
 import { Route, HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 
+import 'bootstrap';
+
 import fontawesome from '@fortawesome/fontawesome';
 // import brands from '@fortawesome/fontawesome-free-brands';
 import faSync from '@fortawesome/fontawesome-free-solid/faSync';
