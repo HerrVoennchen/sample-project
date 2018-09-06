e15c66dae0d9c34f9afd0c78099c52b73efc42f3
Sebastian Krümling <sebastian.kruemling@gmail.com>
2018-09-06 14:06:03 +0200

    stage 3 fetaures

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,16 @@ module.exports = {
         ],
         '@babel/react'
     ],
-    plugins: ['@babel/transform-runtime', 'react-html-attrs', 'transform-class-properties'].concat(
-        debug ? [] : ['@babel/transform-react-inline-elements']
-    )
+    plugins: [
+        '@babel/transform-runtime',
+        'react-html-attrs',
+        'transform-class-properties',
+        '@babel/proposal-json-strings',
+        '@babel/proposal-object-rest-spread',
+        '@babel/proposal-optional-catch-binding',
+        '@babel/proposal-unicode-property-regex',
+        '@babel/syntax-bigint',
+        '@babel/syntax-dynamic-import',
+        '@babel/transform-async-to-generator'
+    ].concat(debug ? [] : ['@babel/transform-react-inline-elements'])
 };
